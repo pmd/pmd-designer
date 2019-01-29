40fd8d6941de9f29d34fa7565d0cbe0fc7c6c6ee
Clément Fournier <clement.fournier76@gmail.com>
2019-01-29 02:42:00 +0100

    Add RulePrioritySlider

--- src/main/java/net/sourceforge/pmd/util/fxdesigner/popups/ExportXPathWizardController.java
+++ src/main/java/net/sourceforge/pmd/util/fxdesigner/popups/ExportXPathWizardController.java
@@ -8,6 +8,9 @@ import static com.github.oowekyala.rxstring.ItemRenderer.asString;
 import static com.github.oowekyala.rxstring.ItemRenderer.indented;
 import static com.github.oowekyala.rxstring.ItemRenderer.surrounded;
 import static com.github.oowekyala.rxstring.ItemRenderer.wrapped;
+import static net.sourceforge.pmd.RulePriority.HIGH;
+import static net.sourceforge.pmd.RulePriority.LOW;
+import static net.sourceforge.pmd.RulePriority.MEDIUM;
 import static net.sourceforge.pmd.util.fxdesigner.util.DesignerUtil.controllerFactoryKnowing;
 import static net.sourceforge.pmd.util.fxdesigner.util.DesignerUtil.getSupportedLanguageVersions;
 import static net.sourceforge.pmd.util.fxdesigner.util.DesignerUtil.rewireInit;
@@ -40,6 +43,7 @@ import net.sourceforge.pmd.util.fxdesigner.util.codearea.SyntaxHighlightingCodeA
 import net.sourceforge.pmd.util.fxdesigner.util.codearea.syntaxhighlighting.XmlSyntaxHighlighter;
 import net.sourceforge.pmd.util.fxdesigner.util.controls.LanguageVersionRangeSlider;
 import net.sourceforge.pmd.util.fxdesigner.util.controls.PropertyTableView;
+import net.sourceforge.pmd.util.fxdesigner.util.controls.RulePrioritySlider;
 
 import com.github.oowekyala.rxstring.LiveTemplate;
 import com.github.oowekyala.rxstring.LiveTemplateBuilder;
@@ -95,7 +99,7 @@ public final class ExportXPathWizardController implements Initializable {
     @FXML
     private TextField messageField;
     @FXML
-    private Slider prioritySlider;
+    private RulePrioritySlider prioritySlider;
     @FXML
     private ChoiceBox<Language> languageChoiceBox;
     @FXML
@@ -219,7 +223,7 @@ public final class ExportXPathWizardController implements Initializable {
 
 
     private Var<RulePriority> priorityProperty() {
-        return Var.doubleVar(prioritySlider.valueProperty()).mapBidirectional(d -> RulePriority.valueOf(d.intValue()), p -> Double.valueOf(p.getPriority()));
+        return prioritySlider.priorityProperty();
     }
 
 
---
+++ src/main/java/net/sourceforge/pmd/util/fxdesigner/util/controls/RulePrioritySlider.java
@@ -0,0 +1,59 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
+package net.sourceforge.pmd.util.fxdesigner.util.controls;
+
+import static net.sourceforge.pmd.RulePriority.HIGH;
+import static net.sourceforge.pmd.RulePriority.LOW;
+import static net.sourceforge.pmd.RulePriority.MEDIUM;
+
+import org.reactfx.value.Var;
+
+import net.sourceforge.pmd.RulePriority;
+import net.sourceforge.pmd.util.fxdesigner.util.DesignerUtil;
+
+import javafx.scene.control.Slider;
+
+
+/**
+ * @author Clément Fournier
+ */
+public class RulePrioritySlider extends Slider {
+
+
+    public RulePrioritySlider() {
+
+        setMin(HIGH.getPriority());
+        setMax(LOW.getPriority());
+        setValue(MEDIUM.getPriority());
+        setMajorTickUnit(1);
+        setBlockIncrement(1);
+        setMinorTickCount(0);
+        setSnapToTicks(true);
+
+        setLabelFormatter(DesignerUtil.stringConverter(
+            d -> {
+                RulePriority rp = RulePriority.valueOf(invert(d.intValue()));
+                return rp != LOW && rp != HIGH && rp != MEDIUM ? "" : rp.getName();
+            },
+            s -> {
+                throw new IllegalStateException("Shouldn't be called");
+            }
+        ));
+    }
+
+    // lowest priority has highest number, so we revert
+    // the indices to get the slider in the right order
+    private int invert(int num) {
+        return LOW.getPriority() + HIGH.getPriority() - num;
+    }
+
+
+    public Var<RulePriority> priorityProperty() {
+        return Var.doubleVar(valueProperty()).mapBidirectional(
+            d -> RulePriority.valueOf(invert(d.intValue())),
+            p -> Double.valueOf(invert(p.getPriority()))
+        );
+    }
+}
