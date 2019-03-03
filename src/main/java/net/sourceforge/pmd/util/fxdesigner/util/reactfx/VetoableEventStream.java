40f03f53a80a5d2ecf268b7691f2ae2ee3700f6a
Clément Fournier <clement.fournier76@gmail.com>
2019-03-03 13:34:03 +0100

    Change param order

--- src/main/java/net/sourceforge/pmd/util/fxdesigner/util/reactfx/VetoableEventStream.java
+++ src/main/java/net/sourceforge/pmd/util/fxdesigner/util/reactfx/VetoableEventStream.java
@@ -41,14 +41,14 @@ public final class VetoableEventStream<I> extends EventStreamBase<I> implements 
     private VetoableEventStream(
         EventStream<I> input,
         BiFunction<I, I, I> vetoableReduction,
-        Predicate<I> shouldWait,
-        Predicate<I> cancelSignal,
+        Predicate<I> isVetoable,
+        Predicate<I> isVeto,
         Function<Runnable, Timer> timerFactory) {
 
         this.input = input;
         this.vetoableReduction = vetoableReduction;
-        this.isVetoable = shouldWait;
-        this.isVetoSignal = cancelSignal;
+        this.isVetoable = isVetoable;
+        this.isVetoSignal = isVeto;
         this.timer = timerFactory.apply(this::handleTimeout);
     }
 
@@ -119,40 +119,39 @@ public final class VetoableEventStream<I> extends EventStreamBase<I> implements 
      * Low-level method to create a vetoable event stream.
      *
      * @param input             Input event stream
+     * @param isVetoable        Predicate that accepts events that should wait for the veto period
+     *                          before it's emitted.
+     * @param isVeto            Predicate that accepts events that cancel a pending vetoable event
      * @param vetoableReduction Reduces two vetoable events, if a new vetoable event is recorded
      *                          while another one was already pending. The pending event is passed
      *                          as the first parameter. If the result of the reduction is vetoable,
      *                          then it's enqueued and treated as pending. Otherwise it's emitted as
      *                          a normal event.
-     * @param isVetoable        Predicate that accepts events that should wait for the veto period
-     *                          before it's emitted.
-     * @param isVeto            Predicate that accepts events that cancel a pending vetoable event
      * @param timerFactory      Factory that produces a timer for the veto period of a vetoable
-     *                          event
      */
     public static <I> AwaitingEventStream<I> vetoableFrom(EventStream<I> input,
-                                                          BiFunction<I, I, I> vetoableReduction,
                                                           Predicate<I> isVetoable,
                                                           Predicate<I> isVeto,
+                                                          BiFunction<I, I, I> vetoableReduction,
                                                           Function<Runnable, Timer> timerFactory) {
         return new VetoableEventStream<>(input, vetoableReduction, isVetoable, isVeto, timerFactory);
     }
 
 
     public static <I> AwaitingEventStream<I> vetoableFrom(EventStream<I> input,
+                                                          Predicate<I> isVetoable,
+                                                          Predicate<I> isVeto,
                                                           BiFunction<I, I, I> vetoableReduction,
-                                                          Predicate<I> shouldWait,
-                                                          Predicate<I> cancelSignal,
                                                           Duration vetoPeriod) {
 
         Function<Runnable, Timer> timerFactory = action -> FxTimer.create(vetoPeriod, action);
-        return vetoableFrom(input, vetoableReduction, shouldWait, cancelSignal, timerFactory);
+        return vetoableFrom(input, isVetoable, isVeto, vetoableReduction, timerFactory);
     }
 
     /**
      * @see ReactfxUtil#vetoableYes(Val, Duration)
      */
     public static AwaitingEventStream<Boolean> vetoableYes(EventStream<Boolean> input, Duration vetoPeriod) {
-        return vetoableFrom(input, (a, b) -> b, b -> b, b -> !b, vetoPeriod);
+        return vetoableFrom(input, b -> b, b -> !b, (a, b) -> b, vetoPeriod);
     }
 }
