2868ca74f6df7a5bc8771dd76d376e6839f23eb2
Juan Martín Sotuyo Dodero <juansotuyo@gmail.com>
2022-08-10 23:03:49 -0300

    Add version check for Mac OSX

--- src/main/java/net/sourceforge/pmd/util/fxdesigner/DesignerStarter.java
+++ src/main/java/net/sourceforge/pmd/util/fxdesigner/DesignerStarter.java
@@ -4,6 +4,10 @@
 
 package net.sourceforge.pmd.util.fxdesigner;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import javax.swing.JOptionPane;
 
 import org.apache.commons.lang3.SystemUtils;
@@ -22,6 +26,13 @@ public final class DesignerStarter {
         "You seem to be missing the JavaFX runtime." + System.lineSeparator()
             + " Please install JavaFX on your system and try again." + System.lineSeparator()
             + " See https://gluonhq.com/products/javafx/";
+
+    private static final String INCOMPATIBLE_JAVAFX =
+        "You seem to be running an older version of JavaFX runtime." + System.lineSeparator()
+            + " Please install the latest JavaFX on your system and try again." + System.lineSeparator()
+            + " See https://gluonhq.com/products/javafx/";
+
+    private static final int MIN_JAVAFX_VERSION_ON_MAC_OSX = 14;
 
     private static final int ERROR_EXIT = 1;
     private static final int OK = 0;
@@ -71,16 +82,42 @@ public final class DesignerStarter {
 
         readParameters(args);
 
-        setSystemProperties();
         launchGui(args);
     }
 
     private static void setSystemProperties() {
         if (SystemUtils.IS_OS_LINUX) {
-            // On Linux, JavaFX renders text poorly by default. These settings help to aleviate the problems.
+            // On Linux, JavaFX renders text poorly by default. These settings help to alleviate the problems.
             System.setProperty("prism.text", "t2k");
             System.setProperty("prism.lcdtext", "true");
         }
+    }
+    
+    private static boolean isCompatibleJavaFxVersion() {
+        if (SystemUtils.IS_OS_MAC_OSX) {
+            final String javaFxVersion = getJavaFxVersion();
+            if (javaFxVersion != null) {
+                final int major = Integer.parseInt(javaFxVersion.split("\\.")[0]);
+                if (major < MIN_JAVAFX_VERSION_ON_MAC_OSX) {
+                    // Prior to JavaFx 14, text on Mac OSX was garbled and unreadable
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private static String getJavaFxVersion() {
+        try (InputStream is = DesignerStarter.class.getClassLoader().getResourceAsStream("javafx.properties")) {
+            final Properties javaFxProperties = new Properties();
+            javaFxProperties.load(is);
+            return (String) javaFxProperties.get("javafx.version");
+        } catch (IOException ignored) {
+            // Can't determine the version
+        }
+
+        return null;
     }
 
     private static String getHelpText(JCommander jCommander) {
@@ -105,9 +142,13 @@ public final class DesignerStarter {
 
     @SuppressWarnings("PMD.AvoidCatchingThrowable")
     private static void launchGui(String[] args) {
+        setSystemProperties();
+
         String message = null;
         if (!isJavaFxAvailable()) {
             message = MISSING_JAVAFX;
+        } else if (!isCompatibleJavaFxVersion()) {
+            message = INCOMPATIBLE_JAVAFX;
         }
 
         if (message != null) {
@@ -115,7 +156,6 @@ public final class DesignerStarter {
             JOptionPane.showMessageDialog(null, message);
             System.exit(ERROR_EXIT);
         }
-
 
         try {
             Application.launch(Designer.class, args);
